2255024fdb946bde967a3676713e9e68b2c7bf40
chelovek-zeml <stranger37561@gmail.com>
2022-08-16 12:01:59 +0300

    add task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max;
+        this.min;
+        this.res;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
+        this.res = 0;
     }
 
     guess() {
-
+        this.res = Math.round((this.max + this.min) / 2);
+        return this.res;
     }
 
     lower() {
-
+        this.max = this.res;
     }
 
     greater() {
-
+        this.min = this.res;
     }
 }
 
